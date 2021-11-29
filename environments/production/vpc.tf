93e00b56db79861438726e2737ba4bc6ab0a500f
madhusudhanarava <madhusudhan.arava@ibexlabs.com>
2021-11-29 20:53:46 +0530

    vpc.tf

--- environments/production/vpc.tf
+++ environments/production/vpc.tf
@@ -4,7 +4,7 @@
 
 # VPC
 provider "aws" {
-  region     = "us-east-2"
+  region     = "eu-central-1"
   access_key = "AKIAXDSL7DJC5HN775LW"
   secret_key = "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"
 }
