02d19b461e4f9ed87135ae114453ff76bb6185fa
madhusudhanarava <madhusudhan.arava@ibexlabs.com>
2021-11-30 13:18:35 +0530

    my files

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
-provider "aws" {
-  region     = "us-east-2"
-  access_key = "AKIAXDSL7DJC5HN775LW"
-  secret_key = "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"
-}
+# provider "aws" {
+#   region     = "us-east-2"
+#   access_key = "AKIAXDSL7DJC5HN775LW"
+#   secret_key = "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"
+# }
